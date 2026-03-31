221a392f258c01b505dbf9c91e0369c95f50f1ed
Luke Dyer <lukedyer24@gmail.com>
2026-03-31 21:51:49 +1100

    Continuing looking at rust

--- RustTest/src/main.rs
+++ RustTest/src/main.rs
@@ -16,7 +16,7 @@ fn main() {
     let times: Array1::<f64> = Array1::<f64>::linspace(0., SIM_LENGTH, NUM_ITERATIONS);
 
     // ConstantPulse
-    let test_constant: ConstantPulse = ConstantPulse { frequency: 1., amplitude: 1., length: 1. };
+    let test_constant: ConstantPulse = ConstantPulse { amplitude: 1., frequency: 1., phase: 1., length: 1. };
     
     println!("{}", inital_state);
 
@@ -27,30 +27,91 @@ fn main() {
 }
 
 trait Pulse {
-    fn get_frequency(&self, t: f64) -> f64;
     fn get_amplitude(&self, t: f64) -> f64;
+    fn get_frequency(&self, t: f64) -> f64;
+    fn get_phase(&self, t: f64) -> f64;
     fn get_length(&self) -> f64;
 }
 
-trait Gate {
-    fn get_frequency(&self, t: f64) -> f64;
-    fn get_amplitude(&self, t: f64) -> f64;
-}
-
 struct ConstantPulse {
-    frequency: f64,
     amplitude: f64,
+    frequency: f64,
+    phase: f64,
     length: f64,
 }
 
 impl Pulse for ConstantPulse {
+    fn get_amplitude(&self, _t: f64) -> f64 {
+        return self.amplitude
+    }
     fn get_frequency(&self, _t: f64) -> f64 {
         return self.frequency
     }
-    fn get_amplitude(&self, _t: f64) -> f64 {
-        return self.amplitude
+    fn get_phase(&self, _t: f64) -> f64 {
+        return self.phase
     }
     fn get_length(&self) -> f64 {
         return self.length
     }
 }
+
+trait Gate {
+    fn get_amplitude(&self, t: f64) -> f64;
+    fn get_frequency(&self, t: f64) -> f64;
+    fn get_phase(&self, t: f64) -> f64;
+    fn get_length(&self) -> f64;
+}
+
+
+struct IdleGate {
+    length: f64,
+}
+
+impl Gate for IdleGate {
+    fn get_amplitude(&self, _t: f64) -> f64 {
+        return 0.
+    }
+    fn get_frequency(&self, _t: f64) -> f64 {
+        return 0.
+    }
+    fn get_phase(&self, _t: f64) -> f64 {
+        return 0.
+    }
+    fn get_length(&self) -> f64 {
+        return self.length
+    }
+}
+
+
+struct PiO2X {}
+
+impl PiO2X {
+    const PI02X_PULSE: ConstantPulse = ConstantPulse { amplitude: 1., frequency: 0., phase: 0., length: 0.5 };
+}
+
+impl Gate for PiO2X {
+    fn get_amplitude(&self, t: f64) -> f64 {
+        return PiO2X::PI02X_PULSE.get_amplitude(t)
+    }
+    fn get_frequency(&self, t: f64) -> f64 {
+        return PiO2X::PI02X_PULSE.get_frequency(t)
+    }
+    fn get_phase(&self, t: f64) -> f64 {
+        return PiO2X::PI02X_PULSE.get_phase(t)
+    }
+    fn get_length(&self) -> f64 {
+        return PiO2X::PI02X_PULSE.get_length()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
